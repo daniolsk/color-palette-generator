748e63a369533e5885b679cee1f9a2479766cf32
daniolsk <danio.skowron@gmail.com>
2025-10-05 17:07:56 +0200

    Change default color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { ColorPicker } from "@/components/ColorPicker";
 import { HsvaColor } from "@/lib/colors";
 
 const Home = () => {
-    const [color, setColor] = useState<HsvaColor>({ h: 13.04, s: 100, v: 59.41, a: 1 });
+    const [color, setColor] = useState<HsvaColor>({ h: 0, s: 70, v: 100, a: 1 });
 
     return (
         <div className="grid grid-cols-2 min-h-screen">
